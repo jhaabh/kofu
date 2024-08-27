ad1c437417a156acc3efd38515afdcb5dd9fff9e
Abhishek Jha <136185915+jhaabh@users.noreply.github.com>
2024-08-27 05:09:48 +0000

    Fix module imports.

--- messor/__init__.py
+++ messor/__init__.py
@@ -3,5 +3,6 @@
 __version__ = '0.1.0'
 
 from .local_threaded_executor import LocalThreadedExecutor
+from .memory import SQLiteMemory
 
-__all__ = ['LocalThreadedExecutor']
+__all__ = ['LocalThreadedExecutor', 'SQLiteMemory']
