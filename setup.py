2c565aa7cca52ecbf41f13390f9dc16bf321bcf3
Abhishek Jha <136185915+jhaabh@users.noreply.github.com>
2024-08-27 12:38:15 +0000

    Bump up version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='kofu',  # Project name
-    version='0.1.0',
+    version='0.2.0',
     description='An execution framework for i/o heavy task with memory persistence and concurrency',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
